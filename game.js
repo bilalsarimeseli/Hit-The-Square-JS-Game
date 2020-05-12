85f3040337ee8df164da4f11acf0a23c29c735e8
Bilal Sarimeseli <45982392+bilalsarimeseli@users.noreply.github.com>
2020-05-12 06:01:11 -0700

    variables and functions written

--- game.js
+++ game.js
@@ -4,3 +4,18 @@ var aHeight;
 var timer;
 var iterations;
 
+window.addEventListener('load', setGameArea);
+function setGameArea(){
+    aWidth = innerWidth;
+    aHeight = innerHeight;
+    aWidth -= 22;
+    aHeight -= 97;
+    document.getElementById('gameArea').style.width = aWidth + 'px';
+    document.getElementById('gameArea').style.height = aHeigth + 'px';
+    document.getElementById('point').addEventListener('click', detectHit);
+}
+
+function detectHit(){
+    score += 1;
+    document.getElementById('scoreBoard').innerHTML = "SCORE " + score;
+}
