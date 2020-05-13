b74b99c8ca83abb44786dd8dabcfcf0b3728bda3
Bilal Sarimeseli <45982392+bilalsarimeseli@users.noreply.github.com>
2020-05-13 00:47:36 -0700

    styles added

--- game.js
+++ game.js
@@ -31,17 +31,19 @@ function movePoint() {
   if (y < 10) y = 10;
   document.getElementById("point").style.left = x + "px";
   document.getElementById("point").style.top = y + "px";
-  if (iterations < 30) timer = setTimeout("movePoint()", 800);
+  if (iterations < 30) timer = setTimeout("movePoint()", 820);
   else {
     document.getElementById("scoreBoard").innerHTML += "     Game Over!";
     document.getElementById("point").removeEventListener("click", detectHit);
     clearTimeout(timer);
   }
+  iterations++;
 }
 
 function titleShift(){
   var xTitle = Math.floor(Math.random() * aWidth);
   if (xTitle < 10) xTitle = 10;
+  console.log(xTitle)
   document.getElementById("pageTitle").style.left = xTitle + "px";
   if(iterations < 30) timer = setTimeout("titleShift()", 800);
 };
